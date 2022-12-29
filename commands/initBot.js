8ef0bae1df7504404b5a87b104fa46e989a1c169
Nevo067 <51850812+Nevo067@users.noreply.github.com>
2022-12-29 02:48:31 +0100

    linked conversation

--- commands/initBot.js
+++ commands/initBot.js
@@ -1,11 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 const axios = require('axios');
 const wait = require('node:timers/promises').setTimeout;
+const { API } = require('../config.json');
+
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('talk')
-        .setDescription('talk with ai')
+        .setName('init')
+        .setDescription('init ai')
         .addStringOption(option => option
             .setName('text')
             .setDescription('Text send to AI')
@@ -13,15 +15,17 @@ module.exports = {
 
     async execute(interaction)
     {
-
         await interaction.deferReply();
         const textToAi = interaction.options.getString('text');
-        const result = await axios.post('http://localhost:8000/test', {
+        console.log(API + 'init');
+        console.log('xxx');
+        const result = await axios.post(API + 'init', {
             text:textToAi,
-
         });
+
         console.log(result);
         await wait(2000);
+        console.log(result.data);
         await interaction.editReply('User: ' + textToAi + '\n\n' + result.data);
 
     },
--- commands/talkTo.js
+++ commands/talkTo.js
@@ -1,11 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
 const axios = require('axios');
 const wait = require('node:timers/promises').setTimeout;
+const { API } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('talk')
-        .setDescription('talk with ai')
+        .setName('talkto')
+        .setDescription('talking to ia with follow-up')
         .addStringOption(option => option
             .setName('text')
             .setDescription('Text send to AI')
@@ -16,11 +17,12 @@ module.exports = {
 
         await interaction.deferReply();
         const textToAi = interaction.options.getString('text');
-        const result = await axios.post('http://localhost:8000/test', {
+        console.log(API + '/talkto');
+        const result = await axios.post(API + 'talkto', {
             text:textToAi,
 
         });
-        console.log(result);
+        console.log(result.data);
         await wait(2000);
         await interaction.editReply('User: ' + textToAi + '\n\n' + result.data);
 
