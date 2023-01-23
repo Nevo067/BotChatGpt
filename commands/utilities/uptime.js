f4f1cfbe1af4cfa3168fd3b3ba436e01c67f459c
William Wats <williamwats98@gmail.com>
2023-01-23 16:29:10 +0100

    add uptime command

---
+++ commands/utilities/uptime.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('uptime')
+        .setDescription('Indique la durée de temps depuis le dernier démarrage du bot'),
+
+    async execute(interaction, client) {
+        const uptime = convertMS(client.uptime);
+        const uptimeText = `${uptime.d} jours ${uptime.h} heures ${uptime.m} minutes ${uptime.s} secondes`;
+
+        const embed = new EmbedBuilder()
+        .setTitle(`__Temps passé depuis le dernier démarrage de ${client.user.username}__`)
+        .setColor('#C6A969')
+        .setTimestamp()
+        .setFooter({ text: 'uptime', iconURL: client.user.displayAvatarURL()})
+        .addFields(
+            { name : 'Durée', value: uptimeText },
+        );
+
+        interaction.reply({ embeds: [embed] });
+
+        function convertMS(ms) {
+            let d, h, m, s;
+            s = Math.floor(ms / 1000);
+            m = Math.floor(s / 60);
+            s = s % 60;
+            h = Math.floor(m / 60);
+            m = m % 60;
+            d = Math.floor(h / 24);
+            h = h % 24;
+            return {
+                d: d,
+                 h: h,
+                 m: m,
+                 s: s,
+            };
+        }
+    },
+};
--- events/InteractionCreate.js
+++ events/InteractionCreate.js
@@ -2,18 +2,19 @@ const { Events } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
-	async execute(interaction) {
+	async execute(interaction, client) {
 		if (!interaction.isChatInputCommand()) return;
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
 		if (!command) {
+			interaction.reply({ content: `La commande "${interaction.commandName}" n\'existe pas` });
 			console.error(`Aucune commande nommée: "${interaction.commandName}" n'a été trouvée.`);
 			return;
 		}
 
 		try {
-			await command.execute(interaction);
+			await command.execute(interaction, client);
 			console.log(`La commande ${interaction.commandName} a fonctionné avec succès`);
 		} catch (error) {
 			console.error(`Une erreur a eu lieu avec la commande: "${interaction.commandName}"`);
